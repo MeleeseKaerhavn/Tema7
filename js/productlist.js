730a039b691cdc5d4d2f5a52661e326a80ffb018
MeleeseKaerhavn <125559675+MeleeseKaerhavn@users.noreply.github.com>
2025-02-05 12:52:23 +0100

    conditional rendering working, return to top btn implemented, mistaked fixed

--- js/productlist.js
+++ js/productlist.js
@@ -1,7 +1,7 @@
 const myCategory = new URLSearchParams(window.location.search).get("category");
 const productContainer = document.querySelector(".choose_grid"); // Min grid container der holder alle mine elementer i produktliste
 
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`) // link til listen med alle productinformationer for alle id
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}&limit=100`) // link til listen med alle productinformationer for alle id
 .then(response => response.json())
 .then (data => showList(data));
 
@@ -9,13 +9,14 @@ function showList(products) {
     let markup = "";
 products.map(product => { 
     markup +=
-    `        <article class="card ${product.discount && "discount"} ${product.soldout && "sold-out"}">
+    `        <article class="card ${product.discount > 0 ? "discount" : ""} ${product.soldout ? "sold-out" : ""}">
                 <a href="product.html?id=${product.id}">
                 <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt=""></a>
+                ${product.discount > 0 ? `<span class="discount-label">-${product.discount}%</span>` : ""}
                 <h4>Model Name: ${product.productdisplayname}</h4>
                 <h5>Type: ${product.articletype} </h5>
                 <h6>Price: ${product.price} </h6>
-                <a class="cart" href="">ADD TO CART</a>
+                ${product.soldout ? `<span class="soldout-label">ADD TO CART</span>` : '<a class="cart" href="#">ADD TO CART</a>'}
             </article>`;
     }).join(""); // samler og gør listen forståeligt uden mærkelige tegn
     productContainer.innerHTML = markup; // redigerer i den HTML du har ovenfor
